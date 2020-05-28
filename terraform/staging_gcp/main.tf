26485c8bde8a3d91142f54e39f2c2895a05ca12a
jacks821 <charlesjackson88@gmail.com>
2020-05-27 20:17:06 -0700

    updated to uncomment some staging stuff

--- terraform/staging_gcp/main.tf
+++ terraform/staging_gcp/main.tf
@@ -19,34 +19,34 @@ module "networking_service_connection" {
   project_id = var.project_id
 }
 
-#module "postgres" {
-#  source        = "../modules/postgres"
-#  zone          = var.zone
-#  region        = var.region
-#  project_id    = var.project_id
-#  database_name = "ask-and-attest-staging"
-#}
-#
-#module "container_registry" {
-#  source     = "../modules/container_registry"
-#  admin_user = "charlie@askandattest.com"
-#  project_id = var.project_id
-#}
-#
-#module "kubernetes_cluster" {
-#  source     = "../modules/google_container_cluster"
-#  region     = var.region
-#  project_id = var.project_id
-#}
-#
-#module "api-ssl-certificate" {
-#  source           = "../modules/ssl_certificate"
-#  certificate_name = "api"
-#  domain_name      = "api.askandattest.net"
-#}
-#
-#module "www-ssl-certificate" {
-#  source           = "../modules/ssl_certificate"
-#  certificate_name = "interface"
-#  domain_name      = "www.askandattest.net"
-#}
+module "postgres" {
+  source        = "../modules/postgres"
+  zone          = var.zone
+  region        = var.region
+  project_id    = var.project_id
+  database_name = "ask-and-attest-staging"
+}
+
+module "container_registry" {
+  source     = "../modules/container_registry"
+  admin_user = "charlie@askandattest.com"
+  project_id = var.project_id
+}
+
+module "kubernetes_cluster" {
+  source     = "../modules/google_container_cluster"
+  region     = var.region
+  project_id = var.project_id
+}
+
+module "api-ssl-certificate" {
+  source           = "../modules/ssl_certificate"
+  certificate_name = "api"
+  domain_name      = "api.askandattest.net"
+}
+
+module "www-ssl-certificate" {
+  source           = "../modules/ssl_certificate"
+  certificate_name = "interface"
+  domain_name      = "www.askandattest.net"
+}
